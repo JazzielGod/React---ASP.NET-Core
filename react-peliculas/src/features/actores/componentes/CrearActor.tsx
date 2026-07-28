10de0acb8f0de51eab23da57d4f84f3df4b70f5c
JazzielGod <jazzielbh@gmail.com>
2026-07-28 13:44:09 -0700

    Agregar componentes para crear y editar actores; implementar formulario y modelo de datos

--- react-peliculas/src/features/actores/componentes/CrearActor.tsx
+++ react-peliculas/src/features/actores/componentes/CrearActor.tsx
@@ -1,5 +1,18 @@
+import type { SubmitHandler } from "react-hook-form";
+import type ActorCreacion from "../modelos/ActorCreacion.model";
+import FormularioActor from "./FormularioActor";
+
+const onSubmit: SubmitHandler<ActorCreacion> = async (data) => {
+    console.log("Creando datos de actor...");
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(data);
+}
+
 export default function CrearActor() {
     return (
-        <h3>Crear Actor</h3>
+        <>
+            <h3>Crear Actor</h3>
+            <FormularioActor onSubmit={onSubmit} />
+        </>
     )
 }
--- react-peliculas/src/features/actores/componentes/EditarActor.tsx
+++ react-peliculas/src/features/actores/componentes/EditarActor.tsx
@@ -1,12 +1,33 @@
 import { useParams } from "react-router";
+import type ActorCreacion from "../modelos/ActorCreacion.model";
+import { useEffect, useState } from "react";
+import Cargando from "../../../componentes/Cargando";
+import type { SubmitHandler } from "react-hook-form";
+import FormularioActor from "./FormularioActor";
 
 export default function EditarActor() {
     const { id } = useParams();
+    const [modelo, setModelo] = useState<ActorCreacion | undefined>(undefined);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setModelo({
+                nombre: "Tom" + id,
+                fechaNacimiento: "1990-01-01"
+            });
+        }, 1000);
+        return () => clearTimeout(timerId);
+    }, [id]);
+    const onSubmit: SubmitHandler<ActorCreacion> = async (data) => {
+        console.log("Editando datos de actor...");
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data);
+    }
 
     return (
         <>
             <h3>Editar Actor</h3>
-            <p>El id del actor es: {id}</p>
+            {modelo ? <FormularioActor modelo={modelo} onSubmit={onSubmit} /> : <Cargando />}
         </>
     )
 }
